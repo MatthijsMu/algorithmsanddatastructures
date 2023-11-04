45443671f51463b516c99db33f320e96e01a6efb
MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
2023-11-04 08:10:36 +0100

    not much yet

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -10,33 +10,66 @@ private:
     Element* next;
   };
 
-  Element* head;
-  Element* tail;
+  Element* front;
+  Element* back;
 
   size_t sz;
 
   Element& traverse_forward(Element& e, size_t steps) {
-    for(size_t i = 0; i < steps && (&e != tail); i++)
+    for(size_t i = 0; i < steps && (&e != back->prev); i++)
       e = e.next;
     return *e;
   }
 
-  Element& traverse_forward(Element* e, size_t steps) {
-    for(size_t i = 0; i < steps && (e != tail); i++)
+  Element& traverse_backward(Element* e, size_t steps) {
+    for(size_t i = 0; i < steps && (e != front->next); i++)
       e = e.next;
+      return *e;
   }
 
 public:
-  Doubly_linked_list () : head{ new Element{
+  Doubly_linked_list () : head{new Element{0,0}}, tail{new Element{0,0}}, sz{0} {
+    head->next = tail;
+    tail->prev = head;
+  }
 
   void push_front(K key, T data) {
     
   }
+
+  void push_back(K key, T data) {
+
+  }
+
+  void front() {
+
+  }
+
+  void back() {
+
+  }
+
+  void pop_front()
+  
+  void pop_back
+
+  void concat_front
+
+  void concat_back
+
+  void reverse
+
+  Element& operator[](size_t get_index) {
+
+  }
+
+  void insert(Element*, size_t insert_index)
 
 ~Doubly_linked_list () {
   
 
   
 }
+
 
     
